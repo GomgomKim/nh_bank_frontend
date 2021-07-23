1e0105b5d85417bd329788998a519b8582c7c017
lee deok ho <dbrwh35@nate.com>
2021-07-23 17:26:07 +0900

    배달내역 컬럼재수정

--- src/pages/delivery/DeliveryList.js
+++ src/pages/delivery/DeliveryList.js
@@ -319,8 +319,22 @@ class DeliveryList extends Component {
         render: (data) => <div>{comma(data)} 원</div>,
       },
       {
-        title: "배달료",
+        title: "기본배달료",
         dataIndex: "basicDeliveryPrice",
+        className: "table-column-center",
+        width: "8%",
+        render: (data) => <div>{comma(data)} 원</div>,
+      },
+      {
+        title: "할증배달료",
+        dataIndex: "extraDeliveryPrice",
+        className: "table-column-center",
+        width: "8%",
+        render: (data) => <div>{comma(data)} 원</div>,
+      },
+      {
+        title: "총배달료",
+        dataIndex: "deliveryPrice",
         className: "table-column-center",
         width: "8%",
         render: (data) => <div>{comma(data)} 원</div>,
