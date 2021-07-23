9f74027d9d985d10dea4c867920f7e8e1c08dc70
@babbbbly <babbbbly@gmail.com>
2021-07-23 16:18:08 +0900

    배달내역 컬럼추가

--- src/pages/settlement/DeliveryCost.js
+++ src/pages/settlement/DeliveryCost.js
@@ -49,22 +49,110 @@ class DeliveryCost extends Component {
     render() {
 
         const columns = [
-
             {
-                title: "배달료",
-                dataIndex: "deliveryFee",
+                title: "가맹점명",
+                dataIndex: "frName",
+                className: "table-column-center",
+            },
+            {
+                title: "라이더명",
+                dataIndex: "riderName",
+                className: "table-column-center",
+            },
+            {
+                title: "접수일시",
+                dataIndex: "startDate",
+                className: "table-column-center",
+            },
+            {
+                title: "완료일시",
+                dataIndex: "endDate",
+                className: "table-column-center",
+            },
+            {
+                title: "도착지",
+                dataIndex: "arrive",
+                className: "table-column-center",
+            },
+            {
+                title: "금액",
+                dataIndex: "price",
                 className: "table-column-center",
                 render: (data) => <div>{comma(data)}원</div>,
             },
             {
-                title: "날짜",
-                dataIndex: "date",
+                title: "배달료",
+                dataIndex: "deliveryPay",
                 className: "table-column-center",
+                render: (data) => <div>{comma(data)}원</div>,
+            },
+            {
+                title: "배달부가세",
+                dataIndex: "deliveryTax",
+                className: "table-column-center",
+                render: (data) => <div>{comma(data)}원</div>,
+            },
+            {
+                title: "배달수수료",
+                dataIndex: "deliveryFee",
+                className: "table-column-center",
+                render: (data) => <div>{comma(data)}원</div>,
             },
 
         ];
+        const expandedRowRender = (record) => {
+            const dropColumns = [
+                {
+                    title: "가맹점명",
+                    dataIndex: "frName",
+                    className: "table-column-center",
+                },
+                {
+                    title: "사업자번호",
+                    dataIndex: "businessNum",
+                    className: "table-column-center",
+                },
+                {
+                    title: "대표자명",
+                    dataIndex: "deliveryFee",
+                    className: "table-column-center",
+                },
+                {
+                    title: "주소",
+                    dataIndex: "addr",
+                    className: "table-column-center",
+                },
+                {
+                    title: "이메일",
+                    dataIndex: "email",
+                    className: "table-column-center",
+                },
+                {
+                    title: "라이더명",
+                    dataIndex: "riderName",
+                    className: "table-column-center",
+                },
+                {
+                    title: "주민번호",
+                    dataIndex: "securityNum",
+                    className: "table-column-center",
+                },
+                {
+                    title: "연락처",
+                    dataIndex: "phoneNum",
+                    className: "table-column-center",
+                },
 
-
+            ];
+            return (
+                <Table
+                    rowKey={(record) => `record: ${record.idx}`}
+                    columns={dropColumns}
+                    dataSource={[record]}
+                    pagination={false}
+                />
+            );
+        };
         return (
             <>
                 <Button className="download-btn"
@@ -85,7 +173,7 @@ class DeliveryCost extends Component {
                     columns={columns}
                     pagination={this.state.pagination}
                     onChange={this.handleTableChange}
-                    expandedRowRender={this.expandedRowRender}
+                    expandedRowRender={expandedRowRender}
                     expandRowByClick={true}
                 />
 
